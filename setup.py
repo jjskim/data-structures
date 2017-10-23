bd77d32bdcdaefb391f634e1dd7415e670689417
Joseph Kim <joseph.kim.kr@gmail.com>
2017-10-23 14:35:41 -0700

    setup commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""Setup module for data-structures package."""
+
+from setuptools import setup
+
+setup(
+    name="data-structures",
+    description="Implementations of various data structures in Python",
+    package_dir={"": "src"},
+    author="Joseph Kim",
+    author_email="joseph.kim.kr@gmail.com",
+    py_modules=[],
+    install_requires=[],
+    extras_require={
+        "test": ["pytest", "pytest-cov", "pytest-watch", "tox"],
+        "development": ["ipython"]
+    },
+    entry_points={
+    }
+)
