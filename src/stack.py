5178f8bc603b9b1fbacc7a6d9b663d7910d05f3b
Joseph Kim <joseph.kim.kr@gmail.com>
2017-10-24 17:25:28 -0700

    added tests and updated README

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,13 @@
-"""."""
+"""Module to define a stack structure."""
 
 from linked_list import LinkedList
 
 
 class Stack(object):
-    """."""
+    """Define a stack class."""
 
     def __init__(self, iterable=()):
-        """."""
+        """Construct an instance of a stack object."""
         self._linked_list = LinkedList()
         if isinstance(iterable, (str, tuple, list)):
             for thing in iterable:
@@ -16,13 +16,13 @@ class Stack(object):
             raise TypeError("Passed an invalid iterable value.")
 
     def push(self, value):
-        """."""
+        """Add the passed value to the top of the stack."""
         self._linked_list.push(value)
 
     def pop(self):
-        """."""
+        """Return the top value from the stock."""
         return self._linked_list.pop()
 
     def __len__(self):
-        """."""
+        """Return the length of the stack."""
         return self._linked_list.size()
