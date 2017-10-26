e15e15f220f05375589c271b298122b38f4bb2c4
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-10-25 20:16:24 -0400

    Fleshed out the remaining methods and are now working on fine tuning and fixing of issues if pressent.

--- src/double_linked_list.py
+++ src/double_linked_list.py
@@ -1,5 +1,7 @@
 """."""
-import linked_list
+from linked_list import LinkedList
+from linked_list import Node
+
 
 class DoublyLinkedNode(Node):
     """."""
@@ -10,22 +12,34 @@ class DoublyLinkedNode(Node):
         super(DoublyLinkedNode, self).__init__(data, next)
 
 
-
 class DoublyLinkedList(LinkedList):
     """."""
+
+    def __init__(self, iterable=()):
+        """."""
+        self.tail = None
+        super(DoublyLinkedList, self).__init__(iterable)
 
 
     def push(self, val):
         """."""
 
-        self.head = DoublyLinkedNode(None, val, self.head)
+        temp = DoublyLinkedNode(None, val, self.head)
+        self.head.prev = temp
+        self.head = temp
+        if self._counter == 0:
+            self.tail = self.head
         self._counter += 1
 
 
     def append(self, val):
         """."""
 
-        self.tail = DoublyLinkedNode(self.tail, val, None)
+        temp = DoublyLinkedNode(self.tail, val, None)
+        self.tail.next = temp
+        self.tail = temp
+        if self._counter == 0:
+            self.head = self.tail
         self._counter += 1
 
 
@@ -34,4 +48,40 @@ class DoublyLinkedList(LinkedList):
 
         val = super.pop()
         self.head.prev = None
+        if self._counter == 0:
+            self.tail = None
         return val
+
+
+    def shift(self):
+        """."""
+        if self.tail is None:
+            raise IndexError('The list is empty')
+        temp = self.tail.data
+        self.tail = self.tail.prev
+        self._counter -= 1
+        if self._counter == 0:
+            self.head = None
+        return temp
+
+
+    def remove(self, val):
+        """."""
+
+        curr = self.head
+
+        for i in range(self._counter):
+            if(curr.data == val):
+                if(curr.prev is None):
+                    self.pop()
+                elif(curr.next is None):
+                    self.shift()
+                else:
+                    curr.prev.next = curr.next
+                    curr.next.prev = curr.prev
+                    self._counter -= 1
+                return
+            curr = curr.next
+        raise ValueError("Passed value is not in list")
+
+
