19d7a8d0ebd16c6d04d05cea5e19255eddb779d0
Joseph Kim <joseph.kim.kr@gmail.com>
2017-10-30 13:42:56 -0700

    middle of writing tests

--- src/deque.py
+++ src/deque.py
@@ -1,57 +1,51 @@
-"""."""
+"""Module to define a dequeue."""
 
 from double_linked_list import DoublyLinkedList
 
 
 class Deque(object):
-    """."""
+    """Define a deque class."""
 
     def __init__(self, iterable=()):
-        """."""
+        """Construct an instance of a deque."""
         if iterable and not hasattr(iterable, "__iter__"):
             raise TypeError("Passed argument is not an iterable value.")
         self._dll = DoublyLinkedList(iterable)
 
-
     def append(self, val):
-        """."""
+        """Append the value to the end of the deque."""
         self._dll.append(val)
 
-
     def appendleft(self, val):
-        """."""
+        """Add the value to the front of the deque."""
         self._dll.push(val)
 
-
-    def pop(self, val):
-        """."""
+    def pop(self):
+        """Remove and return the value at the end of the deque."""
         if self._dll.head is None:
             raise IndexError("Can't pop from an empty deque")
         self._dll.shift()
 
-
-    def popleft(self, val):
+    def popleft(self):
+        """Remove and return the value at the front of the deque."""
         if self._dll.tail is None:
             raise IndexError("Can't popleft from an empty deque")
         self._dll.pop()
 
-
     def peek(self):
-        """."""
+        """Return the value of the end of the deque."""
         if self._dll.tail is not None:
             return self._dll.tail.data
         else:
             return None
 
-
     def peekleft(self):
-        """."""
+        """Return the value at the front of the deque."""
         if self._dll.head is not None:
             return self._dll.head.data
         else:
             return None
 
-
     def size(self):
-        """."""
+        """Return the number of items in the deque."""
         return self._dll.size()
---
+++ src/test_deque.py
@@ -0,0 +1,119 @@
+"""This is the test file for deque.py."""
+
+import pytest
+
+
+@pytest.fixture
+def empty_deque():
+    """Fixture to return an empty deque."""
+    from deque import Deque
+    return Deque()
+
+
+def test_deque_constructs_with_iterable():
+    """Test that deque constructs correctly when passed an iterable."""
+    from deque import Deque
+    a = Deque([1, 2])
+    assert a.size() == len([1, 2])
+
+
+def test_deque_constructs_empty_when_passed_no_arguments(empty_deque):
+    """Test that an empty deque initializes with size 0."""
+    q = empty_deque
+    assert q.size() == 0
+
+
+def test_deque_raises_exception_when_passed_non_iterable():
+    """Test that passing a deque a noniterable raises an exception."""
+    from deque import Deque
+    with pytest.raises(TypeError):
+        Deque(5)
+
+
+def test_popleft_on_empty_deque_raises_exception(empty_deque):
+    """Test that a call on dequeue on empty queue raises exception."""
+    d = empty_deque
+    with pytest.raises(IndexError):
+        d.popleft()
+
+
+def test_append_correctly_adds_item(empty_deque):
+    """Test that append correctly adds an item to the end of the deque."""
+    q = empty_deque
+    prev_size = q.size()
+    q.append(5)
+    assert q.size() == prev_size + 1
+
+
+def test_append_correctly_adds_item_and_can_be_popleft(empty_deque):
+    """Test that after append, popleft correctly removes the item."""
+    q = empty_deque
+    q.append(5)
+    assert q.popleft() == 5
+
+
+def test_append_correctly_adds_item_and_can_be_pop(empty_deque):
+    """Test that after append, pop correctly removes the item."""
+    q = empty_deque
+    q.append(5)
+    assert q.pop() == 5
+
+
+def test_popleft_correctly_removes_item_in_front_of_deque(empty_deque):
+    """Test that after 2 or more appends, popleft removes first item."""
+    q = empty_deque
+    q.append("first")
+    q.append("second")
+    assert q.popleft() == "first"
+
+
+def test_pop_correctly_removes_item_in_back_of_deque(empty_deque):
+    """Test that after 2 or more appends, pop removes last item."""
+    q = empty_deque
+    q.append("first")
+    q.append("second")
+    assert q.pop() == "second"
+
+
+def test_peek_on_empty_deque_returns_none(empty_deque):
+    """Test that peek on an empty deque returns none."""
+    q = empty_deque
+    assert q.peek() is None
+
+
+def test_peek_on_deque_returns_value_at_end_of_queue(empty_deque):
+    """Test that peek on a nonempty deque returns the value of last item."""
+    q = empty_deque
+    q.append(5)
+    q.append(6)
+    assert q.peek() == 6
+
+
+def test_peekleft_on_deque_returns_value_at_front_of_queue(empty_deque):
+    """Test that peekleft on a nonempty deque returns value of first item."""
+    q = empty_deque
+    q.append(5)
+    q.append(6)
+    assert q.peekleft() == 5
+
+
+def test_peek_does_not_remove_item_deque(empty_deque):
+    """Test that peek only returns the value and does not modify deque."""
+    q = empty_deque
+    q.append(1)
+    q.append(2)
+    q.append(3)
+    size_before_peek = q.size()
+    q.peek()
+    assert q.size() == size_before_peek
+
+
+def test_peekleft_does_not_remove_item_deque(empty_deque):
+    """Test that peek only returns the value and does not modify deque."""
+    q = empty_deque
+    q.append(1)
+    q.append(2)
+    q.append(3)
+    size_before_peekleft = q.size()
+    q.peek()
+    assert q.size() == size_before_peekleft
