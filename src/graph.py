9f3a32ae7386e891ea163f8736e9f019a258654b
Joseph Kim <joseph.kim.kr@gmail.com>
2017-11-02 14:21:54 -0700

    docstrings added and setup.py updated

--- src/graph.py
+++ src/graph.py
@@ -1,19 +1,19 @@
-"""."""
+"""Module to define an unweighted, directed graph."""
 
 
 class Graph(object):
-    """."""
+    """Define an unweighted, directed graph class."""
 
     def __init__(self):
-        """."""
+        """Construct a single instance of unweighted, directed graph."""
         self._edges = {}
 
     def nodes(self):
-        """."""
+        """Return a list of nodes in the graph."""
         return self._edges.keys()
 
     def edges(self):
-        """."""
+        """Return a list of all directed edges in the graph as tuples."""
         all_edges = []
         for item in self._edges.keys():
             for edge in self._edges[item]:
@@ -21,24 +21,24 @@ class Graph(object):
         return all_edges
 
     def add_node(self, val):
-        """."""
+        """Add a node to the graph."""
         self._edges.setdefault(val, [])
 
     def add_edge(self, val1, val2):
-        """."""
+        """Add a directed edge to the graph, from node 1 to node 2."""
         self._edges.setdefault(val1, [])
         self._edges.setdefault(val2, [])
         self._edges[val1].append(val2)
 
     def del_edge(self, val1, val2):
-        """."""
+        """Delete the directed edge from the graph."""
         try:
             self._edges[val1].remove(val2)
         except ValueError:
             raise ValueError('No existing edge between nodes.')
 
     def del_node(self, val):
-        """."""
+        """Delete the node from the graph."""
         try:
             self._edges.pop(val, None)
             for node in self._edges.keys():
@@ -48,20 +48,24 @@ class Graph(object):
             raise ValueError('Node does not exist.')
 
     def has_node(self, val):
-        """."""
+        """Return boolean if passed node is in graph."""
         return val in self._edges
 
     def neighbors(self, val):
-        """."""
+        """
+        Return a list of all directed edges starting from passed node.
+        Raise a ValueError if passed node does not exist in graph.
+        """
         if self.has_node(val):
             return self._edges[val]
         raise ValueError('Node does not exist.')
 
     def adjacent(self, val1, val2):
-        """."""
-        if not self.has_node(val1):
-            raise ValueError('Node value1 not present in graph.')
-        elif not self.has_node(val2):
-            raise ValueError('Node value2 not present in graph')
+        """
+        Return a boolean if there exists an edge connecting passed nodes.
+        Raise a ValueError if either of passed nodes do not exist in graph.
+        """
+        if not self.has_node(val1) or not self.has_node(val2):
+            raise ValueError('At least one node not present in graph.')
         else:
-            return val2 in self.neighbors(val1)
+            return val2 in self.neighbors(val1) or val1 in self.neighbors(val2)
